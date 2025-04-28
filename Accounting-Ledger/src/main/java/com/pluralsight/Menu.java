d34911cb673fb4e191b67cbe44903d81b1f58327
Ethan.OToole <eotoole005@gmail.com>
2025-04-28 10:57:30 -0400

    Updated menu displays reports, add deposit, make payment

--- Accounting-Ledger/src/main/java/com/pluralsight/Menu.java
+++ Accounting-Ledger/src/main/java/com/pluralsight/Menu.java
@@ -8,6 +8,7 @@ public class Menu {
         System.out.println("P) Make Payment (Debit)");
         System.out.println("L) Ledger");
         System.out.println("X) Exit");
+        //Takes users input
     }
 
     public void displayLedger() {
@@ -15,5 +16,30 @@ public class Menu {
         System.out.println("D) Display Deposits");
         System.out.println("P) Display Payments");
         System.out.println("R) Display Reports");
+        //Takes users input
     }
+
+    public void displayAddDeposit() {
+        System.out.println("Please enter deposit information below.");
+        System.out.println("Format is description:vendor:amount ");
+        //Takes user input
+    }
+
+    public void displayMakePayment() {
+        System.out.println("Please enter the payment information below. ");
+        System.out.println("Format is description:vendor:amount ");
+        //Takes the users input.
+    }
+
+    public void displayReports() {
+        System.out.println("Which format would you like the reports to be in? ");
+        System.out.println("1) Month To Date ");
+        System.out.println("2) Previous Month ");
+        System.out.println("3) Year To Date ");
+        System.out.println("4) Previous Year ");
+        System.out.println("5) Search by Vendor ");
+        System.out.println("0) Back to Ledger Page");
+        //Takes the users input.
+    }
+
 }
